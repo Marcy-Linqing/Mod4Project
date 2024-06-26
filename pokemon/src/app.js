70dbbc7c22f940f3728c20879a3c679892bded8b
andiizhu <azhu0317@gmail.com>
2024-06-26 14:26:54 -0400

    MVP

--- pokemon/src/app.js
+++ pokemon/src/app.js
@@ -1,20 +1,39 @@
+import { getPokemonList, getPokemon } from "./fetch-functions";
+import { fetchAndRenderPokemonList, renderPokemon } from "./render-functions";
+import { handleSubmit } from "./event-handlers";
+
 export default async function app(appDiv) {
-	const testRoute = async (url) => {
-		const response = await fetch(url);
-		const data = await response.json();
-		console.log("data:", data);
-		return data;
-	};
+	// const testRoute = async (url) => {
+	// 	const response = await fetch(url);
+	// 	const data = await response.json();
+	// 	console.log("data:", data);
+	// 	return data;
+	// };
 
-	const pokemonName = 1;
-	const numberOfPokemon = 20;
-	const url1 = `https://pokeapi.co/api/v2/pokemon/?limit=${numberOfPokemon}`;
-	const url2 = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+	// const pokemonName = 1;
+	// const numberOfPokemon = 20;
+	// const url1 = `https://pokeapi.co/api/v2/pokemon/?limit=${numberOfPokemon}`;
+	// const url2 = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
 
-	testRoute(url1);
-	testRoute(url2);
+	const pokemonList = await getPokemonList();
+	const pokemonListUL = document.querySelector("#pokemon-list");
+	fetchAndRenderPokemonList(pokemonListUL, pokemonList);
+
+	const pokemon = await getPokemon(1);
+	console.log(pokemon);
+	const body = document.querySelector("body");
+	renderPokemon(body, pokemon);
+
+	// console.log(pokemonList);
+
+	// const pokemonData = await getPokemon();
+	// renderPokemon(pokemonData);
+
+	document.querySelector("form").addEventListener("submit", handleSubmit);
+	// testRoute(url1);
+	// testRoute(url2);
 	// const showResult = document.createElement("p");
 	// const textContentResult = await testRoute(url1);
 	// showResult.textContent = textContentResult.results[0].name;
-	appDiv.appendChild(showResult);
+	// appDiv.append(showResult);
 }
---
+++ pokemon/src/render-functions.js
@@ -0,0 +1,53 @@
+export const fetchAndRenderPokemonList = async (pokemonListUL, pokemonList) => {
+	try {
+		pokemonListUL.innerHTML = "";
+
+		for (const pokemon of pokemonList) {
+			// can't use forEach with await inside of it because it won't wait for the asynchronous code to run. Need to use for of loop.
+			const url = pokemon.url;
+			const response = await fetch(url);
+			if (!response.ok) throw new Error("Failed to load Pokemon");
+			const jsonData = await response.json();
+
+			const li = document.createElement("li");
+			const img = document.createElement("img");
+			const p = document.createElement("p");
+			img.src = jsonData.sprites.front_default;
+			p.textContent = jsonData.name;
+			li.append(img, p);
+			pokemonListUL.append(li);
+		}
+	} catch (error) {
+		console.warn(error.message);
+		return null;
+	}
+};
+
+export const renderPokemon = (body, pokemon) => {
+	const h2 = document.createElement("h2");
+	const baseStats = document.createElement("p");
+	const imageDiv = document.createElement("div");
+	const img = document.createElement("img");
+	const statsDiv = document.createElement("div");
+	const p1 = document.createElement("p");
+	const p2 = document.createElement("p");
+	const p3 = document.createElement("p");
+	const p4 = document.createElement("p");
+	const p5 = document.createElement("p");
+	const p6 = document.createElement("p");
+	h2.textContent =
+		pokemon.forms[0].name.slice(0, 1).toUpperCase() +
+		pokemon.forms[0].name.slice(1);
+	baseStats.textContent = `Base Stats:`;
+	img.src = pokemon.sprites.front_default;
+	p1.textContent = pokemon.stats[0].base_stat;
+	p2.textContent = pokemon.stats[1].base_stat;
+	p3.textContent = pokemon.stats[2].base_stat;
+	p4.textContent = pokemon.stats[3].base_stat;
+	p5.textContent = pokemon.stats[4].base_stat;
+	p6.textContent = pokemon.stats[5].base_stat;
+
+	imageDiv.append(img);
+	statsDiv.append(p1, p2, p3, p4, p5, p6);
+	body.append(h2, baseStats, imageDiv, statsDiv);
+};
