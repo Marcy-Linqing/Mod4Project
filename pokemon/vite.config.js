5cddb44a06fbb32672f1599c25131ce79d72ef0b
andiizhu <azhu0317@gmail.com>
2024-07-02 11:11:17 -0400

    finsihed finsihed - thanks izzy

--- pokemon/vite.config.js
+++ pokemon/vite.config.js
@@ -1,9 +1,18 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	// GitHub Pages expects an index.html in the root directory
 	// so just run npm build before pushing to GitHub and this will rebuild our assets to the root
-	build: { outDir: ".." },
+	build: {
+		outDir: "..",
+		rollupOptions: {
+			input: {
+				download: resolve(__dirname, "index.html"),
+				homepage: resolve(__dirname, "pokemon.html"),
+			},
+		},
+	},
 	// needed for github pages just put the repo name here
 	base: "/Mod4Project/",
 });
